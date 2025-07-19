f631b97aec71d9cb621c644341ee29eade84b7cc
MinsooKang <minsu1599@naver.com>
2025-07-19 17:18:17 +0900

    Feat: Add HelloController, DTO, test and cleanup .gitignore

---
+++ src/test/java/com/jojoldu/book/springboot/e2e/HelloE2ETest.java
@@ -0,0 +1,37 @@
+package com.jojoldu.book.springboot.e2e;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+public class HelloE2ETest {
+
+    private static WebDriver driver;
+
+    @BeforeAll
+    static void setup() {
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(
+                "--headless=new",           // 헤드리스(신규)
+                "--disable-gpu",
+                "--no-sandbox",
+                "--disable-dev-shm-usage",
+                "--remote-allow-origins=*"); // ⭐︎ Chrome 115+ 보안 차단 해제
+
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    void helloEndpointDisplaysHello() {
+        driver.get("http://localhost:8080/hello");
+        String body = driver.findElement(By.tagName("body")).getText();
+        Assertions.assertEquals("hello", body);
+    }
+
+    @AfterAll
+    static void teardown() {
+        if (driver != null) driver.quit();
+    }
+}
